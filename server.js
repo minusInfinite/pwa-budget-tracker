f39b84ffe6e773f30f7e599561f15e95d2a533d6
minusInfinite <ashley@minusinfinite.net>
2021-10-15 21:36:17 +1030

    package update

--- server.js
+++ server.js
@@ -1,28 +1,31 @@
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-const compression = require("compression");
+const express = require("express")
+const logger = require("morgan")
+const mongoose = require("mongoose")
+const compression = require("compression")
+require("dotenv").config()
 
-const PORT = 3000;
+const PORT = 3001
 
-const app = express();
+const app = express()
 
-app.use(logger("dev"));
+app.use(logger("dev"))
 
-app.use(compression());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(compression())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-app.use(express.static("public"));
+app.use(express.static("public"))
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose
+    .connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .catch((err) => console.error(err))
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/api.js"))
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+    console.log(`App running on port ${PORT}!`)
+})

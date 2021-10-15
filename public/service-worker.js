b8deedc8c4ef3cf146ed3cd1b4597b7b193de99b
minusInfinite <ashley@minusinfinite.net>
2021-10-15 23:08:07 +1030

    testing indexdb

---
+++ public/service-worker.js
@@ -0,0 +1,66 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/assets/css/style.css",
+    "/assets/js/app.js",
+    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+    "https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap",
+    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+]
+
+const PRECACHE = "precache-v2"
+const RUNTIME = "runtime"
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches
+            .open(PRECACHE)
+            .then((cache) => cache.addAll(FILES_TO_CACHE))
+            .then(self.skipWaiting())
+            .catch((err) => console.log(err))
+    )
+})
+
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener("activate", (event) => {
+    const currentCaches = [PRECACHE, RUNTIME]
+    event.waitUntil(
+        caches
+            .keys()
+            .then((cacheNames) => {
+                return cacheNames.filter(
+                    (cacheName) => !currentCaches.includes(cacheName)
+                )
+            })
+            .then((cachesToDelete) => {
+                return Promise.all(
+                    cachesToDelete.map((cacheToDelete) => {
+                        return caches.delete(cacheToDelete)
+                    })
+                )
+            })
+            .then(() => self.clients.claim())
+    )
+})
+
+self.addEventListener("fetch", (event) => {
+    if (event.request.url.startsWith(self.location.origin)) {
+        event.respondWith(
+            caches.match(event.request).then((cachedResponse) => {
+                if (cachedResponse) {
+                    return cachedResponse
+                }
+
+                return caches.open(RUNTIME).then((cache) => {
+                    return fetch(event.request).then((response) => {
+                        return cache
+                            .put(event.request, response.clone())
+                            .then(() => {
+                                return response
+                            })
+                    })
+                })
+            })
+        )
+    }
+})

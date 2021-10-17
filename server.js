6f7993457c0b28e798e48c74d070416ee00b3540
minusInfinite <ashley@minusinfinite.net>
2021-10-17 17:47:30 +1030

    Adding updates

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose")
 const compression = require("compression")
 require("dotenv").config()
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 const app = express()
 
